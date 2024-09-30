55c865721ffb3320029d70c4eeacb0e6827c2f39
Harshitapattaiya-03 <harshitapattaiya@gmail.com>
2024-09-30 14:45:17 +0530

    passed

--- program2.py
+++ program2.py
@@ -4,7 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        t= 0
+        p = 0
+
+        for char in reversed(s):
+            current_value = roman_map[char]
+
+            if current_value < p:
+                t -= current_value
+            else:
+                t += current_value
+
+            p = current_value
+        
+        return t
 
 
 
